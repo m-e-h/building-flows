9d489ca6baec67573a8b89e7e31683fa873a49bb
m-e-h <info@martyhelmick.com>
2018-02-09 13:50:46 -0500

    gulp

---
+++ gulp/src/build-tasks/headerComment.js
@@ -0,0 +1,19 @@
+const banner = [
+	"/*",
+	"Theme Name: MyTheme",
+	"Theme URI: https://github.com/m-e-h/MyTheme",
+	"Author: Marty Helmick",
+	"Author URI: https://github.com/m-e-h",
+	"Description: Parent theme with many children.",
+	"Version: 1.0.1",
+	"License: GNU General Public License v2 or later",
+	"License URI: http://www.gnu.org/licenses/gpl-2.0.html",
+	"Text Domain: theme",
+	"GitHub Theme URI: https://github.com/m-e-h/MyTheme",
+	"*/",
+	"\n"
+].join("\n");
+
+module.exports = {
+	task: banner
+};
